3dff05df26fd46aa6dec4ec58b7da268bbf18d8d
Ray Fitzgerald <117381503@umail.ucc.ie>
2020-11-18 16:29:37 +0000

    adapted graham scam convex hull

--- src/Order.py
+++ src/Order.py
@@ -1,16 +1,23 @@
 import time
+import random
 
 
 class Order:
 
-    def __init__(self, id, user, address, placed_time, scheduled_time, size):
+    def __init__(self, id, user, coords, placed_time, scheduled_time, size):
 
         self._id = id
         self._user = user
-        self._address = address
+        self._coords = coords
+        self._distance = random.randrange(30)
         self._placed_time = time.time()
         self._scheduled_time = scheduled_time
+        self._time_to_delivery = random.randrange(30)
         self._size = size
+
+    def __str__(self):
+
+        return str(self.id)
 
     @property
     def id(self):
@@ -21,8 +28,8 @@ class Order:
         return self._user
 
     @property
-    def get_address(self):
-        return self._address
+    def coords(self):
+        return self._coords
 
     @property
     def placed_time(self):
@@ -35,3 +42,14 @@ class Order:
     @property
     def size(self):
         return self._size
+
+    @property
+    def distance(self):
+        return self._distance
+
+    @property
+    def time_to_delivery(self):
+        return self._time_to_delivery
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from Van import Van
 from Courier import Courier
-
+from Order import Order
+from random import random, randrange
+from operator import itemgetter
+from grahamscan import GrahamScan
+import numpy as np
 
 all_couriers = list()
 all_orders = list()
@@ -10,10 +14,59 @@ all_vans = list()
 
 def main():
     #create a new van and courier
+    '''
     van1 = Van(100)
     courier1 = Courier('Mike')
     courier1.van = van1
     print(courier1)
+    order1 = Order(12, 'ray', 'Main Road, Cork', '13:05', '14:30', randrange(30))
+    print(order1)
+    '''
+    #create a list of orders
+    orders = create_orders(10)
+
+    print('\nUnsorted orders: ')
+    for order in orders:
+        print(order)
+
+    print('\nsorted orders: ')
+    orders_gch = greedy_construction(orders)
+    for order in orders_gch:
+        print(order[0])
+
+
+    L = GrahamScan(orders)
+    print(L)
+
+
+def create_orders(quantity):
+    orders = list()
+    for i in range (0, quantity):
+        coords = (np.random.randint(0,300),np.random.randint(0,300))
+        orders.append(Order(i, 'ray', coords, '13:05', '14:30', randrange(30)))
+
+    return orders
+
+def greedy_construction(orders):
+
+    unsorted_list = list()
+
+    # create sorted list
+    for order in orders:
+        unsorted_list.append([order, order.distance + order.time_to_delivery])
+
+    return sorted(unsorted_list, key=itemgetter(1))
+
+def calculate_time_ratingt(time_val):
+    current_time = (9, 0)
+    # best 3 hours, worst 0
+    # return a value with 0 as best, 1 as worst
+    # 180 minutes => 100 / 180
+    if time_val > 0:
+
+        difference_hours = (time_val.split(':')[0] - current_time[0]) * 60
+        difference_mins = time_val.split(':')[1] - current_
+    100/12
 
 
 if __name__ == '__main__':
